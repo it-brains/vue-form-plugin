12bd8ef76b02953e98af793f65366ecd50239ac9
Vasiliy Gryaznoy <vasja930403@yandex.ru>
2018-07-31 16:54:47 +0300

    Update requests. Add ability setting of headers

--- src/Form.js
+++ src/Form.js
@@ -7,8 +7,9 @@ export default class Form {
    * Class constructor
    *
    * @param {object} data
+   * @param {object} headers
    */
-  constructor(data) {
+  constructor(data, headers = {}) {
     this.originalData = data;
 
     for (let field in data) {
@@ -17,6 +18,7 @@ export default class Form {
 
     this.errors = new Errors();
     this._processing = false;
+    this._headers = headers;
   }
 
   /**
@@ -24,14 +26,13 @@ export default class Form {
    *
    * @param {string} url
    * @param {string} params
+   * @param {string} headers
    * @param {function} successCallback
    * @param {function} errorCallback
    * @returns {Form}
    */
-  get(url, params = {}, successCallback = null, errorCallback = null) {
-    params = { params };
-
-    this.submit('get', url, params).then(data => {
+  get(url, params = {}, headers = {}, successCallback = null, errorCallback = null) {
+    this.request('get', url, {params, headers}).then(data => {
       for (let field in data) {
         this[field] = data[field];
       }
@@ -77,58 +78,97 @@ export default class Form {
   /**
    * Make POST request
    *
-   * @param {string }url
-   * @returns {*}
+   * @param {string} url
+   * @param {object} headers
+   * @returns {Promise<any>}
    */
-  post(url) {
-    return this.submit('post', url);
+  post(url, headers) {
+    return this.request('post', url, {headers});
   }
 
   /**
    * Make PUT request
    *
    * @param {string} url
-   * @returns {*}
+   * @param {object} headers
+   * @returns {Promise<any>}
    */
-  put(url) {
-    return this.submit('put', url);
+  put(url, headers) {
+    return this.request('put', url, {headers});
   }
 
   /**
    * Make PATCH request
    *
    * @param {string} url
-   * @returns {*}
+   * @param {object} headers
+   * @returns {Promise<any>}
    */
-  patch(url) {
-    return this.submit('patch', url);
+  patch(url, headers) {
+    return this.request('patch', url, {headers});
   }
 
   /**
    * Make DELETE request
    *
    * @param {string} url
-   * @returns {*}
+   * @param {object} headers
+   * @returns {Promise<any>}
    */
-  delete(url) {
-    return this.submit('delete', url);
+  delete(url, headers) {
+    return this.request('delete', url, {headers});
+  }
+
+  /**
+   *
+   * @param {string} url
+   * @param {object} data
+   * @param {object} options
+   * @returns {*[]}
+   */
+  transformData(url, data, options) {
+    const headers = options.headers;
+
+    if (!headers || !headers.hasOwnProperty('Content-Type') || (headers['Content-Type'] !== 'multipart/form-data')) {
+      return [...arguments];
+    }
+
+    let formData = new FormData();
+    for (let property in data) {
+      formData.append(property, data[property]);
+    }
+
+    return [url, formData, options]
   }
 
   /**
    * Submit request to the server
    *
-   * @param requestType
-   * @param url
+   * @param {string} requestType
+   * @param {string} url
+   * @param {object} config
    * @returns {Promise<any>}
    */
-  submit(requestType, url, params = null) {
-    if (!params) {
-      params = this.data();
+  request(requestType, url, config = {}) {
+    let requestData = [url];
+    if (['post', 'put', 'patch'].indexOf(requestType) !== -1) {
+      requestData.push(this.data());
+    }
+
+    let {headers = {}, ...options} = config;
+    options.headers = {
+      ...this._headers,
+      ...headers,
+    };
+
+    requestData.push(options);
+    if (['post', 'put', 'patch'].indexOf(requestType) !== -1) {
+      requestData = this.transformData(...requestData);
     }
 
     this._processing = true;
     return new Promise((resolve, reject) => {
-      axios[requestType](url, params)
+      axios[requestType](...requestData)
         .then(response => {
           this.onSuccess(response.data);
 
